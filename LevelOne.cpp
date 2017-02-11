1f2079429c29585011f4de869a63ee7415264ca0
ses10 <sesma@live.com>
2017-02-10 23:27:02 -0500

    add player object as member

--- LevelOne.cpp
+++ LevelOne.cpp
@@ -1,12 +1,20 @@
 #include "LevelOne.h"
 
-bool LevelOne::init(Input* in, StateManager* m)
+bool LevelOne::init(Input* in, StateManager* m, Game* g)
 {
     input = in;
     setManager(m);
+    screenWidth = g->getGraphics()->getWidth();
+    screenHeight = g->getGraphics()->getHeight();
 
     if(!backgroundImage.load("graphics/levelBackground.bmp"))
         return false;
+
+    if(!playerSprite.load("graphics/player.bmp"))
+        return false;
+
+    player.setX(screenWidth/2 - playerSprite.getWidth()/2);
+    player.setY(550);
 
     return true;
 }
@@ -14,4 +22,5 @@ bool LevelOne::init(Input* in, StateManager* m)
 void LevelOne::draw(Graphics* graphics)
 {
     backgroundImage.draw(0,0,graphics);
+    playerSprite.draw(player.getX(), player.getY(), graphics);
 }
--- LevelOne.h
+++ LevelOne.h
@@ -1,17 +1,23 @@
 #ifndef LEVELONE_H
 #define LEVELONE_H
 
+#include "Game.h"
 #include "GameState.h"
 #include "Graphics.h"
 #include "Image.h"
 #include "Input.h"
 #include "Sound.h"
+#include "Player.h"
 
 class LevelOne : public GameState
 {
 private:
     Input* input;
     Image backgroundImage;
+    Image playerSprite;
+    Player player;
+    int screenWidth;
+    int screenHeight;
 public:
     /**
         Sets up the input and StateManager for instance of a SplashState
@@ -19,10 +25,11 @@ public:
 
         @param in, pointer to the Input class that will handle user input for this state
         @param m, pointer to the StateManager class that will handle this state
+        @param g, pointer to the Game object containing this state
 
         @return true if successful, false otherwise
     */
-    bool init(Input* in, StateManager* m);
+    bool init(Input* in, StateManager* m, Game* g);
 
     /**
         Draw the frame for current State
